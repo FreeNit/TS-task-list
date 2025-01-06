925ed9ba0efc7d3937c07f70041b9cfe52cb0110
Dimon <bolilui.dmytro@gmail.com>
2025-01-06 13:32:35 +0200

    Added getter to format amount of assigned persons

--- src/app.ts
+++ src/app.ts
@@ -166,6 +166,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
   private project: Project;
 
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
 
@@ -178,8 +186,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
   configure(): void {}
   renderContent(): void {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
